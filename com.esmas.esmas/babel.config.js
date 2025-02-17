7001563b8f732b2880ab92fc89a6a7d1dd2a7e3b
Efren Martinez <efrencodes@gmail.com>
2025-02-16 21:20:14 -0600

    feat: Add module resolver plugin for Babel with path aliases

--- com.esmas.esmas/babel.config.js
+++ com.esmas.esmas/babel.config.js
@@ -2,6 +2,30 @@ module.exports = function (api) {
 	api.cache(true)
 	return {
 		presets: ['babel-preset-expo'],
-		plugins: ['react-native-reanimated/plugin'],
+		plugins: [
+			'react-native-reanimated/plugin',
+			[
+				'module-resolver',
+				{
+					extensions: [
+						'.ios.js',
+						'.android.js',
+						'.ios.jsx',
+						'.android.jsx',
+						'.js',
+						'.jsx',
+						'.json',
+						'.ts',
+						'.tsx',
+					],
+					root: ['.'],
+					alias: {
+						'@pages': './app',
+						'@/components': './components',
+						'@/icons': './icons',
+					},
+				},
+			],
+		],
 	}
 }
