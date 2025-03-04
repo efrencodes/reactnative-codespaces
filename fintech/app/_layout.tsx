60de66a5f8c5558be8b421f7de952a15901c36c2
efrencodes <efrencodes@gmail.com>
2025-03-03 23:41:26 -0800

    feat: Add login screen with phone number input and navigation to help

--- fintech/app/_layout.tsx
+++ fintech/app/_layout.tsx
@@ -3,8 +3,8 @@ import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { Stack, useRouter } from "expo-router";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Stack, useRouter, Link } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
@@ -43,6 +43,7 @@ const InitialLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+
       <Stack.Screen
         name="signup"
         options={{
@@ -56,6 +57,37 @@ const InitialLayout = () => {
             </TouchableOpacity>
           ),
         }}
+      />
+
+      <Stack.Screen
+        name="login"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <Link href={"/help"} asChild>
+              <TouchableOpacity>
+                <Ionicons
+                  name="help-circle-outline"
+                  size={34}
+                  color={Colors.dark}
+                />
+              </TouchableOpacity>
+            </Link>
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name="help"
+        options={{ title: "Help", presentation: "modal" }}
       />
     </Stack>
   );
