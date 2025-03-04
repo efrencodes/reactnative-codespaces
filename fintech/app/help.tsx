157d6e4d437113c8e0bcf0767199a37b061ac4dd
efrencodes <efrencodes@gmail.com>
2025-03-03 23:41:32 -0800

    feat: Add help page component with basic layout

---
+++ fintech/app/help.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const PageHelp = () => {
+    return (
+        <View>
+            <Text>Index</Text>
+        </View>
+    )
+}
+
+export default PageHelp;
